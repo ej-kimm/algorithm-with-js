1edda9cab9d5f5f393a0d8abf57c81ffd98deb3f
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-08 09:23:09 +0900

    🧐 Solve : 두 정수 사이의 합

---
+++ Level1/두정수사이의합.js
@@ -0,0 +1,12 @@
+// Solution 1
+function solution(a, b) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum
+}
