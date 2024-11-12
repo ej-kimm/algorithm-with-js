4cffab938ba2b1f6697f2b56806520e9ba9ea1a6
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-12 10:14:49 +0900

    🧐 Solve : 핸드폰 번호 가리기

--- Level1/핸드폰번호가리기.js
+++ Level1/핸드폰번호가리기.js
@@ -2,3 +2,9 @@
 function solution(phone_number) {
   return ('*').repeat(phone_number.length - 4) + phone_number.slice(-4)
 }
+
+// Solution 2
+function solution(phone_number) {
+  let n = phone_number.length;
+  return phone_number.slice(n - 4).padStart(n, '*');
+}
